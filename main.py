ea39d96bb6553a42b5ce2ae1803c7d1cf9d5ade5
chrnk-exe <ivan_kot2001@mail.ru>
2023-06-06 15:34:32 +0300

    simple_reverse_proxy

---
+++ main.py
@@ -0,0 +1,106 @@
+from http.server import BaseHTTPRequestHandler,HTTPServer
+import argparse, os, random, sys, requests
+
+from socketserver import ThreadingMixIn
+import threading
+
+s = requests.Session()
+
+hostname = 'localhost'
+
+def merge_two_dicts(x, y):
+    return x | y
+
+def set_header():
+    headers = {
+        'Host': hostname
+    }
+
+    return headers
+
+class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.0'
+    def do_HEAD(self):
+        self.do_GET(body=False)
+        return
+        
+    def do_GET(self, body=True):
+        sent = False
+        try:
+            url = 'http://{}:8001{}'.format(hostname, self.path)
+            req_header = self.parse_headers()
+            resp = s.get(url, headers=req_header)
+            sent = True
+
+            self.send_response(resp.status_code)
+            self.send_resp_headers(resp)
+            msg = resp.text
+            if body:
+                self.wfile.write(msg.encode(encoding='UTF-8',errors='strict'))
+            return
+        finally:
+            if not sent:
+                self.send_error(404, 'error trying to proxy')
+
+    def do_POST(self, body=True):
+        sent = False
+        try:
+            url = 'http://{}:8001{}'.format(hostname, self.path)
+            req_header = self.parse_headers()
+            content_len = int(req_header['Content-Length'])
+            post_body = self.rfile.read(content_len)
+            resp = s.post(url, data=post_body, headers=req_header)
+            sent = True
+
+            self.send_response(resp.status_code)
+            self.send_resp_headers(resp)
+            if body:
+                self.wfile.write(resp.content)
+            return
+        finally:
+            if not sent:
+                self.send_error(404, 'error trying to proxy')
+
+    def parse_headers(self):
+        req_header = {}
+        local_headers = [string for string in self.headers.as_string().split('\n')]
+        for line in local_headers:
+            line_parts = [o.strip() for o in line.split(':', 1)]
+            if len(line_parts) == 2:
+                req_header[line_parts[0]] = line_parts[1]
+        return req_header
+
+    def send_resp_headers(self, resp):
+        respheaders = resp.headers
+        # print ('Response Header')
+        for key in respheaders:
+            if key not in ['Content-Encoding', 'Transfer-Encoding', 'content-encoding', 'transfer-encoding', 'content-length', 'Content-Length']:
+                # print (key, respheaders[key])
+                self.send_header(key, respheaders[key])
+        self.send_header('Content-Length', len(resp.content))
+        self.end_headers()
+
+def parse_args(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Proxy HTTP requests')
+    parser.add_argument('--port', dest='port', type=int, default=9999,
+                        help='serve HTTP requests on specified port (default: random)')
+    parser.add_argument('--hostname', dest='hostname', type=str, default=hostname,
+                        help=f'hostname to be processd (default: {hostname})')
+    args = parser.parse_args(argv)
+    return args
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+def main(argv=sys.argv[1:]):
+    global hostname
+    args = parse_args(argv)
+    hostname = args.hostname
+    print('http server is starting on {} port {}...'.format(args.hostname, args.port))
+    server_address = ('0.0.0.0', args.port)
+    httpd = ThreadedHTTPServer(server_address, ProxyHTTPRequestHandler)
+    print('http server is running as reverse proxy')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    main()
